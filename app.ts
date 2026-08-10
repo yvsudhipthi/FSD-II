0ec5fb03276b9d36c99422e74c6cf0875c70306e
yvsudhipthi <yvsudhipthi@gmail.com>
2026-08-10 21:25:40 +0530

    Delete app.ts

--- app.ts
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const app=express();
-const PORT=3000;
-app.get('/',(req,res)=>{
-    resizeBy.send('<h1>Welcome SVECW</h1><p>You have reached the Home Page.<p>');
-});
